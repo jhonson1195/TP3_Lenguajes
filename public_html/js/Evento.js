ae826d3528fddb603c89213a7f4a8f3761bc4c7a
jhonson <jhonson1195@gmail.com>
2015-11-20 21:24:17 -0600

    agregar evento 99%

--- public_html/js/Evento.js
+++ public_html/js/Evento.js
@@ -1,140 +1,79 @@
 
 var CLIENT_ID = '439520072436-uf4gs63ivhul10p46vmqtjlso2da4vr6.apps.googleusercontent.com';
 
-var SCOPES = ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/drive"];
+var SCOPES = ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/plus.me"];
 
       /**
        * Check if current user has authorized this application.
        */
-      function checkAuth() {
-        gapi.auth.authorize(
+function checkAuth() {
+gapi.auth.authorize(
           {
             'client_id': CLIENT_ID,
             'scope': SCOPES.join(' '),
             'immediate': true
-          }, handleAuthResult);
+          });
       }
+      
+  //window.onload = checkAuth;
+function comprobarCheck(){
+    
+    if(document.getElementById("c1").checked){alert("ok");return '1';};
+    if(document.getElementById("c2").checked){return '2';}
+    if(document.getElementById("c3").checked){return '3';}
+    if(document.getElementById("c4").checked){return '4';}
+    if(document.getElementById("c5").checked){return '5';}
+    if(document.getElementById("c6").checked){return '6';}
+    if(document.getElementById("c7").checked){return '7';}
+    if(document.getElementById("c8").checked){return '8';}
+    if(document.getElementById("c9").checked){return '1';}
+    if(document.getElementById("c10").checked){return '10';}
+    if(document.getElementById("c11").checked){return '11';}
+};
+var Correos = [];
 
-      /**
-       * Handle response from authorization server.
-       *
-       * @param {Object} authResult Authorization result.
-       */
-      function handleAuthResult(authResult) {
-        var authorizeDiv = document.getElementById('authorize-div');
-        if (authResult && !authResult.error) {
-          // Hide auth UI, then load client library.
-          authorizeDiv.style.display = 'none';
-          loadCalendarApi();
-        } else {
-          // Show auth UI, allowing the user to initiate authorization by
-          // clicking authorize button.
-          authorizeDiv.style.display = 'inline';
-        }
-      }
-
-      /**
-       * Initiate auth flow in response to user clicking authorize button.
-       *
-       * @param {Event} event Button click event.
-       */
-      function handleAuthClick(event) {
-        gapi.auth.authorize(
-          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-          handleAuthResult);
-        return false;
-      }
-
-      /**
-       * Load Google Calendar client library. List upcoming events
-       * once client library is loaded.
-       */
-      function loadCalendarApi() {
-        gapi.client.load('calendar', 'v3', listUpcomingEvents);
-      }
-
-      /**
-       * Print the summary and start datetime/date of the next ten events in
-       * the authorized user's calendar. If no events are found an
-       * appropriate message is printed.
-       */
-      function listUpcomingEvents() {
-        var request = gapi.client.calendar.events.list({
-          'calendarId': 'primary',
-          'timeMin': (new Date()).toISOString(),
-          'showDeleted': false,
-          'singleEvents': true,
-          'maxResults': 10,
-          'orderBy': 'startTime'
-        });
-
-        request.execute(function(resp) {
-          var events = resp.items;
-          appendPre('Upcoming events:');
-
-          if (events.length > 0) {
-            for (i = 0; i < events.length; i++) {
-              var event = events[i];
-              var when = event.start.dateTime;
-              if (!when) {
-                when = event.start.date;
-              }
-              appendPre(event.summary + ' (' + when + ')')
-            }
-          } else {
-            appendPre('No upcoming events found.');
-          }
-
-        });
-      }
-
-      /**
-       * Append a pre element to the body containing the given message
-       * as its text node.
-       *
-       * @param {string} message Text to be placed in pre element.
-       */
-      function appendPre(message) {
-        var pre = document.getElementById('output');
-        var textContent = document.createTextNode(message + '\n');
-        pre.appendChild(textContent);
-      }
-
-        
+function AgregarCorreo(){
+    Correos.push({'email': document.getElementsByName("Invitado")[0].value});
+    document.getElementsByName("Invitado")[0].value="";
+};
         
 // FUNCTION TO INSERT EVENT
-   function insertEvent(calendar) {
-   	var resource = {
-        'summary': 'Google I/O 2015',
-  'location': '800 Howard St., San Francisco, CA 94103',
-  'description': 'A chance to hear more about Google\'s developer products.',
-  'start': {
-    'dateTime': '2015-11-14T09:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'end': {
-    'dateTime': '2015-11-17T17:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=2'
-  ],
-  'attendees': [
-    {'email': 'jhonson1195@gmail.com'}
-  ],
-  'reminders': {
+function AgregarEvento() {
+   var Cuerpo = {'summary': document.getElementsByName("summary")[0].value};
+
+if(document.getElementsByName("summary")[0].value===""){alert("Por favor gregue un titulo"); return;};
+if(document.getElementsByName("end")[0].value==="" && document.getElementsByName("start")[0].value==="" ){alert("Por favor gregue una fecha de inicio o fin "); return;};
+
+
+if(document.getElementsByName("hora1")[0].value===""){
+    Cuerpo.start = {'date': document.getElementsByName("start")[0].value}
+}else{Cuerpo.start = {'dateTime': document.getElementsByName("start")[0].value + 'T'+ document.getElementsByName("hora1")[0].value+':00-06:00'}};
+
+if(document.getElementsByName("hora2")[0].value===""){
+    Cuerpo.end = {'date': document.getElementsByName("end")[0].value}
+}else{Cuerpo.end = {'dateTime': document.getElementsByName("end")[0].value + 'T'+ document.getElementsByName("hora2")[0].value+':00-06:00'}};
+
+if(!(document.getElementsByName("location")[0].value==="")){
+    Cuerpo.location = document.getElementsByName("location")[0].value;
+};
+if(!(document.getElementsByName("description")[0].value==="")){Cuerpo.description = document.getElementsByName("description")[0].value;};
+if(!(document.getElementsByName("reminders")[0].value==="")){
+    Cuerpo.reminders = {
     'useDefault': false,
     'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10}
-    ]
-  }
+      {'method': 'email', 'minutes': parseInt(document.getElementsByName("reminders")[0].value) * 60},
+      {'method': 'popup', 'minutes': parseInt(document.getElementsByName("reminders")[0].value) * 60}
+    ]        
+};}
+else{Cuerpo.reminders = {'useDefault': false}};
 
-      };
-     gapi.client.load('calendar', 'v3', function() {  
+if(!(Correos.length===0)){Cuerpo.attendees = Correos;}
+Cuerpo.colorId=comprobarCheck();
+
+    gapi.client.load('calendar', 'v3', function() {  
        var request = gapi.client.calendar.events.insert({
-         'calendarId': calendar,
-	 'resource': resource
+         'calendarId': 'primary',
+	 'resource': Cuerpo
        });
      request.execute(function(resp) {
        console.log(resp);
@@ -150,15 +89,18 @@ var SCOPES = ["https://www.googleapis.com/auth/calendar", "https://www.googleapi
      
    } 
    
-   function makeApiCall() {
+   function CargarNombreUsuario() {
   gapi.client.load('plus', 'v1').then(function() {
     
     
     var request = gapi.client.plus.people.get({
   'userId' : 'me'
     });
-
+  
+  
   request.execute(function(resp) {
+      
+  document.getElementById("Label1").innerHTML = resp.displayName;
   console.log('ID: ' + resp.id);
   console.log('Display Name: ' + resp.displayName);
   console.log('Image URL: ' + resp.image.url);
--- public_html/js/java.js
+++
@@ -1,9 +0,0 @@
-
-
-/* global form, texto1 */
-
-function Mesaje() {
-    var hola= document.getElementsByName("texto2")[0].value;
-    console.log(hola);
-    alert(hola); 
-}
